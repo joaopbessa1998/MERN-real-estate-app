50422095cc01a35dd1fee4280daef0a8e68d3d85
João Pedro Bessa <joaopbessa1998@gmail.com>
2023-10-18 16:17:19 +0100

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  //   .connect(process.env.MONGO)
+  .connect(
+    "mongodb+srv://imjotap:mern-real-estate-app@mern-real-estate-app.ercmpm2.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
