9264e24038da0fcff7942019efc6fcc1a75b737b
João Pedro Bessa <joaopbessa1998@gmail.com>
2023-10-18 18:04:35 +0100

    create sign up API route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js'
+
 dotenv.config();
 
 mongoose
@@ -18,9 +20,12 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
 
 
-app.use("/api/user", userRouter)
+app.use("/api/user", userRouter);
+app.use('/api/auth', authRouter);
